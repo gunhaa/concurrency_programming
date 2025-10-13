949ad869a79de00e0d4224a908d8d4f44ae22044
gunha <wh8299@gmail.com>
2025-10-13 23:27:38 +0900

    chore: 폴더 구조 정리, c4_bug 추가

--- c3_synchronized1/rust_synchronized1/src/main.rs
+++ c3_synchronized1/rust_synchronized1/src/main.rs
@@ -1,9 +1,8 @@
 mod mutex;
 mod bakery_algorithm;
-mod dining_philosophers;
+
 
 fn main() {
     // mutex::run_mutex_example();
     // bakery_algorithm::run_bakery_algorithm_example();
-    dining_philosophers::default_implements_philosopers();
 }
---
+++ c4_bug/rust_bug/src/main.rs
@@ -0,0 +1,5 @@
+mod dining_philosophers;
+    
+fn main() {
+    dining_philosophers::default_implements_philosopers();
+}
